4ede5590717b769d02c2e83044f1d85b2d8b0250
Jesse Plym <bgu254@myy.haaga-helia.fi>
2022-10-25 15:25:22 +0300

    booking

--- src/main/java/hh/palvelinohjelmointi/travel/web/BookingController.java
+++ src/main/java/hh/palvelinohjelmointi/travel/web/BookingController.java
@@ -22,11 +22,19 @@ public class BookingController {
 
 	// this method shows all bookings made by user
 	// only the owner can make changes
-	@GetMapping("/bookings")
+	@GetMapping("/ownbookings")
 	public String showUserBookings(Model model, Principal principal) {
 		String username = principal.getName();
 		User user = userRepo.findByUsername(username);
 		model.addAttribute("bookings", bookingRepo.findByUser(user));
 		return "booking";
 	}
+
+	// shows all bookings for testing
+	@GetMapping("/allbookings")
+	public String showAllBookings(Model model) {
+		model.addAttribute("bookings", bookingRepo.findAll());
+		return "booking";
+	}
+
 }
