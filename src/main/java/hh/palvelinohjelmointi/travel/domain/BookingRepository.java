a2415bbd09023cd59992ebfee8152d1f0859d8f9
Jesse Plym <bgu254@myy.haaga-helia.fi>
2022-11-02 17:03:40 +0200

    booking editing done

--- src/main/java/hh/palvelinohjelmointi/travel/domain/BookingRepository.java
+++ src/main/java/hh/palvelinohjelmointi/travel/domain/BookingRepository.java
@@ -3,8 +3,15 @@ package hh.palvelinohjelmointi.travel.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BookingRepository extends CrudRepository<Booking, Long> {
 
-	List<Booking> findByUser(User user);
+	// List<Booking> findByUser(User user);
+
+	List<Booking> findByUser(@Param("user") User user);
+
+	List<Booking> findByTrip(@Param("trip") Trip trip);
 }
---
+++ src/main/java/hh/palvelinohjelmointi/travel/web/BookingRestController.java
@@ -0,0 +1,21 @@
+package hh.palvelinohjelmointi.travel.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hh.palvelinohjelmointi.travel.domain.Booking;
+import hh.palvelinohjelmointi.travel.domain.BookingRepository;
+
+@RestController
+public class BookingRestController {
+
+	@Autowired
+	private BookingRepository repository;
+
+	@RequestMapping("/bookings")
+	public Iterable<Booking> getBookings() {
+		return repository.findAll();
+	}
+
+}
